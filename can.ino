dc5530468dbb7bf517f5b8a5120d31d850ec3c22
Mark Maher <66829236+mark-maher-moris@users.noreply.github.com>
2023-02-25 23:01:21 +0200

    Update can.ino

--- can.ino
+++ can.ino
@@ -1,8 +1,38 @@
+
+#include <WiFi.h>
+#include <FirebaseESP32.h>
+
 #include <LiquidCrystal.h> //include LCD library (standard library)
 #include <Keypad.h> //include keypad library - first you must install library (library link in the video description)
 
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#include <FirebaseESP8266.h>
+#endif
+#include <addons/TokenHelper.h>
+#include <addons/RTDBHelper.h>
+
+
 #define redLED 10 //define the LED pins
 #define greenLED 11
+
+
+
+
+FirebaseData fbdo;
+
+FirebaseAuth auth;
+FirebaseConfig config;
+
+unsigned long sendDataPrevMillis = 0;
+
+unsigned long count = 0;
+
+
+
+
+
+
 
 char* actNumber ="1234"; //create a password
 int pozisyon = 0; //keypad position
@@ -41,6 +71,37 @@ void setup(){
   pinMode(redLED, OUTPUT);  //set the LED as an output
   pinMode(greenLED, OUTPUT);
   setLocked (true); //state of the password
+  
+  
+  
+  
+  sensors.begin();
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+  Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
+  config.api_key = API_KEY;
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+  config.database_url = DATABASE_URL;
+  config.token_status_callback = tokenStatusCallback; 
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+
+  Firebase.setDoubleDigits(5);
+  
+
+  
+  
+  
 }
 
 void loop(){
@@ -74,6 +135,29 @@ void loop(){
   // print the value to Serial Monitor
   Serial.print("distance: ");
   Serial.print(distance_cm);
+    if (distance_cm < 20){
+    cutsNumber ++ ;
+      // saffar
+    }
+    
+      Firebase.getDouble(fbdo, F("/users/1234/points"), &points);
+    // get user points from firebase
+    points = points + cutsNumber ;
+    // set user new points for firebase 
+    // 
+          Firebase.getDouble(fbdo, F("/users/1234/points"), &points);
+  if (Firebase.ready() && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0))
+  {
+    sendDataPrevMillis = millis();
+    Firebase.setDouble(fbdo, F("/users/1234/points"),points);
+       
+}    
+    
+    
+    
+
+    
+
   Serial.println(" cm");
     delay(800);
     lcd.clear();
@@ -86,7 +170,7 @@ void loop(){
     setLocked (false);
     lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print("** Code:9999 **");
+    lcd.print("** Code: 1234**");
     delay(3000);
  
 
@@ -124,12 +208,12 @@ void setLocked(int locked){
 #include <addons/TokenHelper.h>
 #include <addons/RTDBHelper.h>
 
-#define WIFI_SSID ""
-#define WIFI_PASSWORD ""
+#define WIFI_SSID "Saif's iPhone"
+#define WIFI_PASSWORD "666666666"
 #define API_KEY "AIzaSyBAtY159LKZWaFq5OSjsTpiuuPDigdZv-Q"
 #define DATABASE_URL "https://default-rtdb.firebaseio.com/"
 #define USER_EMAIL "@gmail.com"
-#define USER_PASSWORD "MaherDevice"
+#define USER_PASSWORD "TrashCan"
 
 
 FirebaseData fbdo;
